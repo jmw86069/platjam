77a31bdfdd26b42ab2da8beb744154462a57a73c
jmw86069 <jmw86069@gmail.com>
2025-07-23 12:48:14 -0400

    Commit original version for proper comparison.

--- R/platjam-deeptools.R
+++ R/platjam-deeptools.R
@@ -11,7 +11,7 @@ deepTools_matrix2nmat <- function
 (x=NULL,
  filename=NULL,
  signal_name=NULL,
- target_name=NULL,
+ target_name="target",
  background=0,
  smooth=FALSE,
  target_is_single_point=FALSE,
@@ -23,104 +23,45 @@ deepTools_matrix2nmat <- function
  verbose=FALSE,
  ...)
 {
-   # import YAML using the first line only
-   x1 <- readLines(filename, n=1)
-   xyaml <- yaml::read_yaml(text=gsub("^@", "", x1))
-   samples <- xyaml[["sample_labels"]];
-   bodysize <- xyaml[["body"]]; # optional for gene body width?
-   binsize <- xyaml[["bin size"]];
-   upstream <- xyaml[["upstream"]];
-   downstream <- xyaml[["downstream"]];
-
-   sort_regions <- xyaml[["sort regions"]]; # descend
-   sort_using <- xyaml[["sort using"]]; # mean
-
-   if (length(target_name) == 0) {
-      target_name <- xyaml[["ref point"]];
-   }
-   signal_name <- samples;
-
-   # row group boundaries (row number at each group change)
-   group_boundaries <- xyaml[["group_boundaries"]];
-   group_labels <- xyaml[["group_labels"]];
-
-   # sample boundaries - column number for each column grouping?
-   sample_boundaries <- xyaml[["sample_boundaries"]];
-
-   if (TRUE %in% verbose) {
-      jamba::printDebug("deepTools_matrix2nmat(): ",
-         "\nsamples:", samples,
-         "\nrow groups:", group_labels,
-         "\nbinsize:", binsize,
-         "\nupstream:", upstream,
-         "\nbody:", bodysize,
-         "\ndownstream:", downstream)
-   }
-
-
+   x1 <- readLines(filename, n=1);
+   xyaml <- yaml::read_yaml(text=gsub("^@", "", x1));
+   samples <- xyaml$sample_labels;
+   samples;
+   binsize <- xyaml$`bin size`;
+   binsize;
+   upstream <- xyaml$upstream;
+   upstream;
+   group_boundaries <- xyaml$group_boundaries;
+   sample_boundaries <- xyaml$sample_boundaries;
    x <- data.table::fread(text=readLines(filename),
       skip=1,
       header=FALSE,
-      sep="\t")
-   rownames(x) <- jamba::makeNames(x[[4]]);
-   if (TRUE %in% verbose) {
-      jamba::printDebug("deepTools_matrix2nmat(): ",
-         "Loaded data.")
-   }
+      sep="\t");
+   rownames(x) <- x[[4]];
 
-   mats <- as.matrix(x[, -1:-6, drop=FALSE])
-
-   mat_n <- ceiling(ncol(mats)/length(samples));
+   mats <- as.matrix(x[,-1:-6,drop=FALSE]);
+   mat_n <- ceiling(ncol(mats) / length(samples));
 
    ## required attributes
-   starts <- seq(from=-upstream,
-      by=binsize,
-      length.out=mat_n);
-   ends <- starts + binsize - 1;
+   starts <- seq(from=-upstream, by=binsize, length.out=mat_n);
    upstream_index <- which(starts < 0);
    target_index <- which(starts == 0);
    downstream_index <- which(starts > 0);
-   if (any(bodysize) > 0) {
-      new_colnames <- paste0("u", seq_len(ncol(im)));
-   } else {
-      new_colnames <- paste0(starts, ":", ends);
-   }
-   # target_name <- xyaml$group_labels;
+   target_name <- xyaml$group_labels;
 
-   # column groupings (optional)
-   mat_split <- rep(
-      rep(samples, each=mat_n),
-      length.out=ncol(mats))
-   mat_split <- factor(mat_split, levels=unique(samples));
-
-   # make list of columns in group order as they appear
-   colnames_l <- split(colnames(mats), mat_split)
-
-   # split columns to individual matrices
-   mat_l <- lapply(seq_along(colnames_l), function(k) {
+   mat_split <- rep(rep(samples, each=mat_n), length.out=ncol(mats));
+   colnames_l <- split(colnames(mats), mat_split);
+   mat_l <- lapply(seq_along(colnames_l), function(k){
       i <- colnames_l[[k]];
-      im <- mats[, i, drop=FALSE]
-      colnames(im) <- new_colnames;
-      rownames(im) <- rownames(x)
-      attr(im, "signal_name") <- samples[k]
-      attr(im, "target_name") <- target_name
-      attr(im, "upstream_index") <- upstream_index
-      attr(im, "target_index") <- target_index
-      attr(im, "downstream_index") <- downstream_index
-      im
-   })
+      im <- mats[,i];
+      colnames(im) <- paste0("u", seq_len(ncol(im)));
+      rownames(im) <- x[[4]];
+      attr(im, "signal_name") <- samples[k];
+      attr(im, "target_name") <- target_name;
+      attr(im, "upstream_index") <- upstream_index;
+      attr(im, "target_index") <- target_index;
+      attr(im, "downstream_index") <- downstream_index;
+      im;
+   });
 
-   # add anno_df if relevant
-   if (length(group_boundaries) > 2) {
-      group_sizes <- diff(group_boundaries)
-      anno_df <- data.frame(check.names=FALSE,
-         row.names=rownames(mat_l[[1]]),
-         name=rownames(mat_l[[1]]),
-         group=rep(group_labels, group_sizes))
-      attr(mat_l, "anno_df") <- anno_df;
-   }
-   attr(mat_l, "rownames") <- rownames(mat_l[[1]]);
-   attr(mat_l, "colnames") <- new_colnames;
-
-   mat_l;
 }
